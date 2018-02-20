db19a2522f73cf8977b509bb68107d85a580e36d
williambl <william@williambl.com>
2018-02-20 17:10:41 +0000

    Added Unity Standard Assets movement

--- holo-match/Assets/Scripts/PlayerController.cs
+++ holo-match/Assets/Scripts/PlayerController.cs
@@ -23,11 +23,5 @@ public class PlayerController : NetworkBehaviour {
 	void Update () {
             if (!isLocalPlayer)
                 return;
-
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
-
-            transform.Rotate(0, x, 0);
-            transform.Translate(0, 0, z);        
 	}
 }
