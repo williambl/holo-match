c923ed18d7b81298bd6e5576634f669170b66275
williambl <william@williambl.com>
2018-03-12 16:52:14 +0000

    Added a Mesh to the Weapon

--- holo-match/Assets/Scripts/Weapon/Weapon.cs
+++ holo-match/Assets/Scripts/Weapon/Weapon.cs
@@ -1,8 +1,12 @@
+using UnityEngine;
+
 public class Weapon {
 
     public EnumAmmoType ammoType;
     public EnumWeaponType type;
     public EnumFireType fireType;
+
+    public Mesh mesh;
 
     public int ammo;
     public int maxAmmo;
