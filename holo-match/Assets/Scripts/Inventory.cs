f0e489590cf1da9513780aed95393376b5b94157
williambl <william@williambl.com>
2018-06-01 00:28:55 +0100

    Added a simple weapon switching system

--- holo-match/Assets/Scripts/Inventory.cs
+++ holo-match/Assets/Scripts/Inventory.cs
@@ -10,14 +10,63 @@ public class Inventory : NetworkBehaviour {
     [SyncVar]
     public int weaponCount = 3;
 
-    void Update () {
+    public GameObject weapon0;
+    public GameObject weapon1;
+    public GameObject weapon2;
 
+    void Start () {
+        EquipWeapon();
+    }
+
+    void Update () {
+        if (!isLocalPlayer)
+            return;
+
+        int previousEquippedWeapon = equippedWeapon;
+
+        //Change weapon by scrolling
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
             equippedWeapon++;
         else if (Input.GetAxis("Mouse ScrollWheel") < 0)
             equippedWeapon--;
 
         //Wraps the equipped weapon around
-        equippedWeapon = (equippedWeapon + weaponCount) % weaponCount; 
+        equippedWeapon = (equippedWeapon + weaponCount) % weaponCount;
+
+        //If our equipped weapon has changed, update it
+        if (previousEquippedWeapon != equippedWeapon)
+            EquipWeapon();
+    }
+
+    void EquipWeapon () {
+        switch (equippedWeapon) {
+            case 0:
+                weapon0.SetActive(true);
+                weapon1.SetActive(false);
+                weapon2.SetActive(false);
+                break;
+            case 1:
+                weapon0.SetActive(false);
+                weapon1.SetActive(true);
+                weapon2.SetActive(false);
+                break;
+            case 2:
+                weapon0.SetActive(false);
+                weapon1.SetActive(false);
+                weapon2.SetActive(true);
+                break;
+            default:
+                /*
+                 * If we ever have an equipped weapon outside of the range
+                 * 0..2, then something has gone terribly wrong...
+                 */
+
+                Debug.unityLogger.Log(LogType.Error,
+                        "You have an equipped weapon outside of the range 0..2! Something has gone terribly wrong...");
+                weapon0.SetActive(true);
+                weapon1.SetActive(false);
+                weapon2.SetActive(false);
+                break;
+        }
     }
 }
