33f7254667fc7e5e60c919956bd7f8e8a08714f0
williambl <william@williambl.com>
2018-06-27 19:22:05 +0100

    Main menu now fully functional

---
+++ holo-match/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MainMenuController : MonoBehaviour {
+
+    public Button host;
+    public Button join;
+    public Button exit;
+    public InputField joinIP;
+
+    void Start () {
+        host.onClick.AddListener(Host);
+        join.onClick.AddListener(Join);
+        exit.onClick.AddListener(Exit);
+    }
+	
+    void Host () {
+        NetworkManager.singleton.StartHost();
+    }
+
+    void Join () {
+        NetworkManager.singleton.networkAddress = joinIP.text == "" ? "localhost" : joinIP.text;
+        NetworkManager.singleton.StartClient();
+    }
+
+    void Exit () {
+        Application.Quit();
+    }
+}
