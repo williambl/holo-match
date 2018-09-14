9e073c7ad44bc25491373ab670bca0fe70acdaae
williambl <william@williambl.com>
2018-09-15 00:20:05 +0100

    Add HoloMod abstract class and CoreMod subclass

---
+++ holo-match/Assets/Mods/CoreMod.cs
@@ -0,0 +1,14 @@
+﻿public class CoreMod : HoloMod {
+
+    public string name = "CoreMod";
+
+    public string version = "v0.0.1";
+
+    public override void RegisterWeapons(WeaponManager manager) {
+    
+    }
+
+    public override void RegisterMaps(MapManager manager) {
+    
+    }
+}
---
+++ holo-match/Assets/Scripts/ModManager/HoloMod.cs
@@ -0,0 +1,10 @@
+﻿public abstract class HoloMod {
+
+    public string name;
+
+    public string version;
+
+    public abstract void RegisterWeapons(WeaponManager manager);
+
+    public abstract void RegisterMaps(MapManager manager);
+}
