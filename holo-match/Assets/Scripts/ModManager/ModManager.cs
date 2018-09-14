e99ca00666f9df47077b7776a3f9e0041a21134a
williambl <william@williambl.com>
2018-09-15 00:32:08 +0100

    Add ModManager, which finds subclasses of HoloMod

---
+++ holo-match/Assets/Scripts/ModManager/ModManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+public class ModManager : MonoBehaviour {
+
+    private List<Type> modRegistry = new List<Type>();
+
+    void Awake () {
+
+        foreach(var asm in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            foreach (var type in asm.GetTypes())
+            {
+                if (type.BaseType == typeof(HoloMod)) {
+                    Debug.Log(type.Name);
+                    modRegistry.Add(type);
+                }
+            }
+        }
+    }
+}
