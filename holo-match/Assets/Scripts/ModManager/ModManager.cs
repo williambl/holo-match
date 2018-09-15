7e8b86e61109b2ebfc3eb39eb7165bb615df3433
williambl <william@williambl.com>
2018-09-15 15:16:00 +0100

    Add mod loading support

--- holo-match/Assets/Scripts/ModManager/ModManager.cs
+++ holo-match/Assets/Scripts/ModManager/ModManager.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Reflection;
 using System;
 using UnityEngine;
 
 public class ModManager : MonoBehaviour {
 
     private List<Type> modRegistry = new List<Type>();
+    private List<AssetBundle> assetBundleRegistry = new List<AssetBundle>();
 
     void Awake () {
 
+        LoadAllMods();
         foreach(var asm in AppDomain.CurrentDomain.GetAssemblies())
         {
             foreach (var type in asm.GetTypes())
@@ -25,6 +29,21 @@ public class ModManager : MonoBehaviour {
         RegisterWeapons(modRegistry);
     }
 
+    private void LoadAllMods () {
+        IEnumerable<String> modDirs = Directory.EnumerateDirectories(Path.Combine(Application.dataPath, "Mods"));
+        foreach (String modDir in modDirs) {
+            String modName = Path.GetDirectoryName(modDir);
+
+            if (File.Exists(Path.Combine(modDir, modName+".dll")))
+            {
+                Assembly.Load(File.ReadAllBytes(Path.Combine(modDir, modName+".dll")));
+
+                foreach (string assetBundlePath in Directory.EnumerateFiles(modDir, "*.assetbundle")) {
+                    assetBundleRegistry.Add(AssetBundle.LoadFromFile(assetBundlePath));
+                }
+            }
+        }
+    }
 
     private void RegisterMaps (List<Type> registry) {
         foreach (var mod in registry) {
